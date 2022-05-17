1cc82729f1ede86de68e48ec9992b6e6d86b8c8d
hashirn <hashirn8119@gmail.com>
2022-05-18 01:43:12 +0530

    Fixed embed generating for anime largely..

--- makembed.js
+++ makembed.js
@@ -2,26 +2,42 @@ const { MessageEmbed } = require("discord.js");
 
 const embed = new MessageEmbed();
 
-const makeAnimembed = (
+const makeAnimembed = ({
   color,
-  desc,
   title,
+  MediaUrl,
+  description,
   thumbnail,
   avgScore,
+  status,
   episodes,
-  status
-) => {
-  embed
-    .setColor(color)
-    .setDescription(desc)
-    .setTitle(title)
-    .setThumbnail(thumbnail)
-    .addField("Avg. score: ", `${avgScore}%`, true)
-    .addField("# of episodes: ", `${episodes}`, true)
-    .addField("Status: ", `\`${status}\``, true);
-  /* embed.setFooter({
-    text: `${format} • ${seasons} ${seasonYear}`,
-    iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}.png`, */
+  id,
+  interaction,
+}) => {
+  return (
+    embed
+      .setColor(color)
+      .setTitle(title)
+      .setURL(MediaUrl)
+      /* .setAuthor({
+      name: "Some name",
+      iconURL: "https://i.imgur.com/AfFp7pu.png",
+      url: "https://discord.js.org",
+    }) */
+      .setDescription(description)
+      .setThumbnail(thumbnail)
+      .addFields(
+        { name: "Average Score", value: `${avgScore}`, inline: true },
+        { name: "Status", value: `${status}`, inline: true },
+        { name: "Episodes", value: `${episodes}`, inline: true }
+      )
+      .setImage(`https://img.anili.st/media/${id}`)
+      .setTimestamp()
+      .setFooter({
+        text: `${interaction.user.tag}`,
+        iconURL: `${interaction.user.avatarURL({ format: "png", size: 32 })}`,
+      })
+  );
 };
 
 module.exports = { makeAnimembed };
