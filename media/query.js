af6cf1a1bcd65695699bc556f34310988ae30d0e
hashirn <hashirn8119@gmail.com>
2022-05-18 01:40:06 +0530

    Updated query , added episodes,medium img

--- media/query.js
+++ media/query.js
@@ -1,18 +1,21 @@
 module.exports = `query ($search: String) {
-    Media(search: $search,type:ANIME) {
-      id
-      siteUrl
-      title {
-        english
-      }
-      coverImage {
-        large
-      }
-      status(version: 2)
-      description(asHtml: true)
-      averageScore
+  Media(search: $search, type: ANIME) {
+    id
+    siteUrl
+    title {
+      english
     }
+    episodes
+    coverImage {
+      color
+      medium
+    }
+    status(version: 2)
+    description(asHtml: true)
+    averageScore
   }
+}
+
   `;
 /* , type: $type 
 , $type: MediaType
