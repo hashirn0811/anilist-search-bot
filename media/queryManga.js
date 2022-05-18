9da0f7c3fcb527ee65b9c450a4129ac4da1b0a1b
hashirn <hashirn8119@gmail.com>
2022-05-18 18:43:52 +0530

    init

---
+++ media/queryManga.js
@@ -0,0 +1,18 @@
+module.exports = `query ($search: String) {
+    Media(search: $search, type: MANGA) {
+      title {
+        english
+        romaji
+      }
+      description(asHtml: true)
+      format
+      status(version: 2)
+      chapters
+      averageScore
+      genres
+      coverImage {
+        medium
+      }
+    }
+  }
+  `;
