f601baae3dcf4223668d44f5ecdacb5637957a79
hashirn <hashirn8119@gmail.com>
2022-05-18 07:22:42 +0530

    init

---
+++ handlers/handler.js
@@ -0,0 +1,10 @@
+const fs = require(`node:fs`);
+module.exports = async (client) => {
+  const command = fs.readdirSync(`./misc/`).map((cmd) => {
+    let file = require(`../misc/${cmd}`);
+    client.commands.set(file.name, file);
+    if (file.aliases) {
+      file.aliases.map((p) => client.aliases.set(p, file));
+    }
+  });
+};
