6930dff6c0947ba76f30521352ddb17af37233d4
hashirn <hashirn8119@gmail.com>
2022-05-18 17:25:19 +0530

    eval works

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ const path = require("node:path");
 
 require("dotenv").config();
 const { Client, Collection, Intents } = require("discord.js");
-
+const { clean } = require(`./misc/clean`);
 const prefix = process.env.PREFIX || "!";
 const token = process.env.TOKEN;
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const ownerId = process.env.ownerId;
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_BANS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
+});
 
 //load commands from /commands
 client.commands = new Collection();
+
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
@@ -42,9 +55,26 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args));
   }
 }
+/* ["handler"].forEach((handler) => require(`./handlers/${handler}.js`)(client)); */
+//eval
+client.on(`messageCreate`, async (message, ...args) => {
+  if (message.content.startsWith("!eval")) {
+    if (!ownerId.includes(message.author.id)) return;
+    let codein = args.join(" ");
+    try {
+      let code = eval(codein);
+      if (typeof code !== "string")
+        code = require("util").inspect(code, { depth: 0 });
+      message.channel.send(`\`\`\`js\n${code}\n\`\`\``);
+    } catch (e) {
+      return message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
+    }
+  }
+});
 
-["handler"].forEach((handler) => require(`./handlers/${handler}.js`)(client));
-
+process.on("unhandledRejection", (e) => {
+  console.error(`Unhandled Rejection ${e}`);
+});
 //load commands dynamically
 /* client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
