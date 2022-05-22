ce81b6f15a0032b394ee6fc9ef401321da1579aa
hashirn <hashirn8119@gmail.com>
2022-05-23 02:21:27 +0530

    Embeds , try catch

--- commands/testComm.js
+++ commands/testComm.js
@@ -31,12 +31,41 @@ module.exports = {
     const query = interaction.options.getString("query") || "null";
     console.log(`query :${query} type:${type}`);
     let res = "";
+
     if (type === "MANGA") {
       res = await makeRequest(mangaQuery, query);
-      interaction.reply(`${JSON.stringify(res)}`);
+      //interaction.reply(`${JSON.stringify(res)}`);
     } else {
-      res = await makeRequest(animeQuery, query);
-      interaction.reply(`${JSON.stringify(res)}`);
+      try {
+        res = await makeRequest(animeQuery, query);
+        const media = res.Media;
+        console.log(media);
+        let fixDisc = striptags(media.description);
+        fixDisc =
+          fixDisc.length > 400
+            ? `${fixDisc
+                .substring(0, 400)
+                .trim()
+                .replace(/(&quot\;)/g, '"')}...`
+            : `${fixDisc}`;
+        let embed = "";
+        embed = makeAnimembed({
+          color: media.coverImage.color,
+          title: media.title.english ?? media.title.romaji,
+          MediaUrl: media.siteUrl,
+          description: fixDisc,
+          thumbnail: media.coverImage.large,
+          avgScore: media.averageScore,
+          status: media.status,
+          episodes: media.episodes,
+          format: media.format,
+          id: media.id,
+          interaction: interaction,
+        });
+        await interaction.reply({ embeds: [embed] });
+      } catch (error) {
+        console.error(error);
+      }
     }
   },
 };
