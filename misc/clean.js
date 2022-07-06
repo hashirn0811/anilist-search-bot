d5a192473732c320c109b632ecdef1ffd2351d17
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-07-07 00:42:17 +0530

    init

--- misc/clean.js
+++
@@ -1,9 +0,0 @@
-const clean = async (text) => {
-  if (text && text.constructor == "Promise") text = await text;
-
-  if (typeof text !== "string")
-    text = require("util").inspect(text, { depth: 1 });
-  return text;
-};
-
-module.exports = { clean };
