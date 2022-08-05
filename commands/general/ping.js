ff88f0ae540f2517ab7844a2967026933dd2d698
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-08-05 23:38:45 +0530

    ping init

---
+++ commands/general/ping.js
@@ -0,0 +1,36 @@
+const Command = require(`../../Structures/Command`);
+const { EmbedBuilder, SlashCommandBuilder } = require(`discord.js`);
+const { stripIndents } = require(`common-tags`);
+
+class Ping extends Command {
+  constructor(client) {
+    // props = {
+    //   data: new SlashCommandBuilder().setName('ping').setDescription('ping'),
+    //   usage: 'ping',
+    //   category: 'Info',
+    //   permissions: ['Use Application Commands', 'Send Messages', 'Embed Links'],
+    // };
+    super(client, {
+      data: new SlashCommandBuilder().setName('ping').setDescription('ping'),
+      usage: 'ping',
+      category: 'Info',
+      permissions: ['Use Application Commands', 'Send Messages', 'Embed Links'],
+    });
+  }
+  async run(client, interaction) {
+    const now = Date.now();
+    await interaction.deferReply();
+
+    const embed = new EmbedBuilder()
+      .setAuthor({
+        name: `${client.user.username}'s ping`,
+        iconURL: client.user.displayAvatarURL({ size: 2048 }),
+      })
+      .setColor('Blue').setDescription(stripIndents`**Roundtrip : ${Math.round(
+      Date.now() - now
+    )} ms
+    **API : ${Math.round(client.ws.ping)} ms`);
+
+    return await interaction.followUp({ embeds: [embed] });
+  }
+}
