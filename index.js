cf3af5a18c372845726374791050cf253aea5a20
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-08-05 22:36:51 +0530

    Edit Index

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const { Tweek } = require(`./Structures/Client`);
 
 const client = new Tweek();
 
-client._init();
+client.login();
+
+process.on('unhandledRejection', (err) => console.error(err.stack));
+process.on('uncaughtException', (err) => console.error(err.stack));
