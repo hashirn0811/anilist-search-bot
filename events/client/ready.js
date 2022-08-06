5e29afe9229326d1a2788657d01cf8f149c8ef82
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-08-06 13:16:59 +0530

    minor changes

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,15 +1,15 @@
 const Event = require('../../Structures/Event');
 
 module.exports = class ReadyEvent extends Event {
-	constructor(client) {
-		super(client, {
-			name: 'ready',
-			once: true,
-		});
-	}
-	async run(client) {
-		console.log(
-			`Logged in as ${client.user.username} with ${client.users.cache.size} users and ${client.guilds.cache.size} guilds`
-		);
-	}
+  constructor(client) {
+    super(client, {
+      name: 'ready',
+      once: true,
+    });
+  }
+  async run(client) {
+    console.log(
+      `Logged in as ${client.user.username} with ${client.users.cache.size} users and ${client.guilds.cache.size} guilds`
+    );
+  }
 };
--- events/guild/interactionCreate.js
+++ events/guild/interactionCreate.js
@@ -1,37 +1,31 @@
-const Command = require('../../Structures/Command');
+const Event = require('../../Structures/Event');
 const { InteractionType } = require('discord.js');
 
-module.exports = class InteractionCreate extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'interactionCreate',
-		});
-	}
-	async run(client, interaction) {
-		if (interaction.type === InteractionType.ApplicationCommand) {
-			const command = client.commands.get(interaction.commandName);
+module.exports = class InteractionCreate extends Event{
+  constructor(client) {
+    super(client, {
+      name: 'interactionCreate',
+      once: false,
+    });
+  }
+  async run(interaction) {
+    const client = this.client;
 
-			if (interaction.user.bot) return;
-			if (
-				!interaction.inGuild() &&
-        interaction.type === InteractionType.ApplicationCommand
-			)
-				return interaction.reply({
-					content: 'You must be in a server to use commands.',
-				});
-			if (!command)
-				return (
-					(await interaction.reply('Command Unavailable')) &&
-          client.commands.delete(interaction.commandName)
-				);
-			try {
-				command.run(client, interaction);
-			} catch (error) {
-				console.log(error);
-				await interaction.reply({
-					content: `An error occurred.\n\n**\`${error.message}\`**`,
-				});
-			}
-		}
-	}
+    if (interaction.type === InteractionType.ApplicationCommand) {
+      const command = client.commands.get(interaction.commandName);
+      console.log(command);
+      if (interaction.user.bot) return;
+      if (!interaction.inGuild() && interaction.type === InteractionType.ApplicationCommand) return interaction.reply({ content: 'You must be in a server to use commands.' });
+
+      if (!command) return interaction.reply({ content: 'This command is unavailable.', ephemeral: true }) && client.commands.delete(interaction.commandName);
+
+      try {
+        command.run(client, interaction);
+      }
+      catch (e) {
+        console.log(e);
+        return interaction.reply({ content: `An error has occurred.\n\n**\`${e.message}\`**` });
+      }
+    }
+  }
 };
