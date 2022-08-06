4a5966b0e81715f9fa97129399d6e8a2a21b9ae9
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-08-06 13:15:41 +0530

    Read message intent

--- Structures/Client.js
+++ Structures/Client.js
@@ -1,8 +1,8 @@
 const {
-	Client,
-	Collection,
-	GatewayIntentBits,
-	Partials,
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Partials,
 } = require('discord.js');
 const CommandHandler = require('../handlers/Command');
 const EventHandler = require('../handlers/Event');
@@ -10,40 +10,41 @@ const Util = require('./Util');
 require('dotenv').config();
 
 class Tweek extends Client {
-	constructor(props) {
-		if (!props) props = {};
-		props.partials = [
-			Partials.GuildMember,
-			Partials.Message,
-			Partials.Channel,
-			Partials.User,
-		];
-		props.intents = [
-			GatewayIntentBits.Guilds,
-			GatewayIntentBits.GuildMembers,
-			GatewayIntentBits.GuildMessages,
-			GatewayIntentBits.GuildPresences,
-			GatewayIntentBits.GuildBans,
-			GatewayIntentBits.GuildEmojisAndStickers,
-			GatewayIntentBits.GuildIntegrations,
-		];
+  constructor(props) {
+    if (!props) props = {};
+    props.partials = [
+      Partials.GuildMember,
+      Partials.Message,
+      Partials.Channel,
+      Partials.User,
+    ];
+    props.intents = [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.GuildBans,
+      GatewayIntentBits.GuildEmojisAndStickers,
+      GatewayIntentBits.GuildIntegrations,
+      GatewayIntentBits.MessageContent,
+    ];
 
-		super(props);
-		this.config = require('../config');
-		this.token = process.env.TOKEN;
-		this.dbID = process.env.DB;
-		this.commands = new Collection();
-		this.events = new Collection();
-		this.util = new Util(this);
+    super(props);
+    this.config = require('../config');
+    this.token = process.env.TOKEN;
+    this.dbID = process.env.DB;
+    this.commands = new Collection();
+    this.events = new Collection();
+    this.util = new Util(this);
 
-		new EventHandler(this).load('../events/');
-		new CommandHandler(this).load('../commands/');
-	}
-	async login() {
-		return super.login(process.env.TOKEN);
-	}
-	fetchCommand(cmd) {
-		return this.commands.get(cmd);
-	}
+    new EventHandler(this).load('../events/');
+    new CommandHandler(this).load('../commands/');
+  }
+  async login() {
+    return super.login(process.env.TOKEN);
+  }
+  fetchCommand(cmd) {
+    return this.commands.get(cmd);
+  }
 }
 module.exports.Tweek = Tweek;
