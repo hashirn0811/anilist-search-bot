36f84257855a7125baf9cdac5281a35e12c95df3
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-08-06 13:13:47 +0530

    Fix wrong property and method extraction

--- handlers/Command.js
+++ handlers/Command.js
@@ -3,37 +3,37 @@ const { join, resolve } = require('path');
 const { readdir, lstat } = require('fs/promises');
 
 module.exports = class CommandClass {
-	constructor(client) {
-		this.client = client;
-	}
-	async load(dir) {
-		const filePath = join(__dirname, dir);
-		const files = await readdir(filePath);
+  constructor(client) {
+    this.client = client;
+  }
+  async load(dir) {
+    const filePath = join(__dirname, dir);
+    const files = await readdir(filePath);
 
-		for (const file of files) {
-			const stat = await lstat(join(filePath, file));
-			if (stat.isDirectory()) this.load(join(dir, file));
-			if (file.endsWith('.js')) {
-				const Cmd = require(resolve(filePath, file));
+    for (const file of files) {
+      const stat = await lstat(join(filePath, file));
+      if (stat.isDirectory()) this.load(join(dir, file));
+      if (file.endsWith('.js')) {
+        const Cmd = require(resolve(filePath, file));
 
-				if (Cmd.prototype instanceof BaseCommand) {
-					const cmd = new Cmd(this.client);
+        if (Cmd.prototype instanceof BaseCommand) {
+          const cmd = new Cmd(this.client);
 
-					const cmdData = cmd.data.toJSON();
+          const cmdData = cmd.data.toJSON();
 
-					const cmdSet = {
-						name: cmdData.name,
-						description: cmdData.description,
-						options: cmdData.options,
-						defaultPermission: cmdData.defaultPermission,
-						usage: cmdData.usage,
-						category: cmdData.category,
-						run: cmdData.run,
-						permissions: cmdData.permissions,
-					};
-					this.client.commands.set(cmdSet.name, cmdSet);
-				}
-			}
-		}
-	}
+          const cmdSet = {
+            name: cmdData.name,
+            description: cmdData.description,
+            options: cmdData.options,
+            defaultPermission: cmdData.default_Permission,
+            usage: cmd.usage,
+            category: cmd.category,
+            permissions: cmd.permissions,
+            run: cmd.run
+          };
+          this.client.commands.set(cmdSet.name, cmdSet);
+        }
+      }
+    }
+  }
 };
