8f04f57e23b1480b0ae7d860094afdfcb92c6a62
evelyn <84616056+RedLotus811@users.noreply.github.com>
2022-08-11 18:15:40 +0530

    small Changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const { Tweek } = require('./Structures/Client');
+const { Tweek } = require('./Structures/Tweek');
 
 const client = new Tweek();
 
 (async()=>{
   await client.initDB();
   await client.login();
+  await client.sendMessage(`955296933705044018`,`test`,true);
 })();
 process.on('unhandledRejection', (err) => console.error(err.stack));
 process.on('uncaughtException', (err) => console.error(err.stack));
